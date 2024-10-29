22df4f343a4ed1714acf612ef6fa12424ecbd6f0
gianttelepathicsquid <kevinxrp@gmail.com>
2024-10-28 22:09:36 -0700

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import WarehouseGame from './components/WarehouseGame';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-indigo-50 via-purple-50 to-pink-50 p-4">
+      <WarehouseGame />
+    </div>
+  );
+}
+
+export default App;
